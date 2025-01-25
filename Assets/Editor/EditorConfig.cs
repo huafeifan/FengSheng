b99d91acc531779944e49851b406fb9b9707214e
17 <76463683+huafeifan@users.noreply.github.com>
2025-01-25 11:51:20 +0800

    编辑器新建lua文件

--- Assets/Editor/EditorConfig.cs
+++ Assets/Editor/EditorConfig.cs
@@ -3,8 +3,8 @@ using UnityEditor;
 
 public class EditorConfig : Editor
 {
-    [MenuItem("Assets/Create/New lua", false, 1)]
-    public static void CreateLua()
+    [MenuItem("Assets/Create/New Lua/Scene", false, 1)]
+    public static void CreateLua_Scene()
     {
         string selectedPath = "Assets";
         if (Selection.assetGUIDs.Length > 0)
@@ -19,7 +19,158 @@ public class EditorConfig : Editor
                 selectedPath = Path.Combine(Path.GetDirectoryName(selectedPath), "NewLuaFile.lua.txt");
             }
         }
-        File.WriteAllText(selectedPath, string.Empty);
+        File.WriteAllText(selectedPath, GetNewLuaText(NewLuaText.Scene));
         AssetDatabase.Refresh();
     }
+
+    [MenuItem("Assets/Create/New Lua/Layer", false, 1)]
+    public static void CreateLua_Layer()
+    {
+        string selectedPath = "Assets";
+        if (Selection.assetGUIDs.Length > 0)
+        {
+            selectedPath = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
+            if (Directory.Exists(selectedPath))
+            {
+                selectedPath = Path.Combine(selectedPath, "NewLuaFile.lua.txt");
+            }
+            else
+            {
+                selectedPath = Path.Combine(Path.GetDirectoryName(selectedPath), "NewLuaFile.lua.txt");
+            }
+        }
+        File.WriteAllText(selectedPath, GetNewLuaText(NewLuaText.Layer));
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Assets/Create/New Lua/Class", false, 1)]
+    public static void CreateLua_Class()
+    {
+        string selectedPath = "Assets";
+        if (Selection.assetGUIDs.Length > 0)
+        {
+            selectedPath = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
+            if (Directory.Exists(selectedPath))
+            {
+                selectedPath = Path.Combine(selectedPath, "NewLuaFile.lua.txt");
+            }
+            else
+            {
+                selectedPath = Path.Combine(Path.GetDirectoryName(selectedPath), "NewLuaFile.lua.txt");
+            }
+        }
+        File.WriteAllText(selectedPath, GetNewLuaText(NewLuaText.Class));
+        AssetDatabase.Refresh();
+    }
+
+    public enum NewLuaText
+    {
+        None,
+        Scene,
+        Layer,
+        Class
+    }
+
+    private static string GetNewLuaText(NewLuaText type)
+    {
+        switch (type)
+        {
+            case NewLuaText.None:
+                return string.Empty;
+            case NewLuaText.Scene:
+                return 
+@"local ui = {}
+                        
+local InitUI = function()
+
+end
+
+local AddUIEvent = function()
+
+end
+
+local RemoveUIEvent = function()
+
+end
+
+local AddListener = function()
+
+end
+
+local RemoveListener = function()
+
+end
+
+function start()
+    InitUI()
+    AddUIEvent()
+    AddListener()
+end
+
+function ondestroy()
+    RemoveUIEvent()
+    RemoveListener()
+end";
+            case NewLuaText.Layer:
+            return 
+@"local ui = {}
+                        
+local InitUI = function()
+
+end
+
+local AddUIEvent = function()
+
+end
+
+local RemoveUIEvent = function()
+
+end
+
+local AddListener = function()
+
+end
+
+local RemoveListener = function()
+
+end
+
+function awake()
+	InitUI()
+end
+
+function onenable()
+	AddUIEvent()
+	AddListener()
+end
+
+function ondisable()
+	RemoveUIEvent()
+	RemoveListener()
+end
+
+function ondestroy()
+	RemoveUIEvent()
+	RemoveListener()
+end";
+                case NewLuaText.Class:
+                    return 
+@"NewClassName = {}
+NewClassName.__index = NewClassName
+
+function NewClassName:New(gameObject)
+	local item = {}
+	item.gameObject = gameObject
+	item.transform = gameObject.transform
+	item.ui = {}
+	
+	setmetatable(item, NewClassName)
+	return item
+end";
+                default:
+                    return string.Empty;
+        }
+    }
+
+
 }
